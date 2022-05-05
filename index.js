6faadc192a981d65973af5db4b0306bb802b0473
Abram Shook <ajshook77@gmail.com>
2022-05-05 14:20:10 -0500

    commit tree 941bb9851f4e136993d69d0c13d8cae6af468ae0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+let head = document.querySelector('head')
+head.append(newHeader)
+
+// let h1 = 'victory'
+// element.append(newHeader)
+
+// const element = document.getElementById('h1')
+// element.textContent = 'Abram is the champion'
